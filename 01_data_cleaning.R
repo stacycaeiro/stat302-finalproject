70c25d2833d16d3265b097062a5b1765f2a815ab
stacycaeiro <stacycaeiro2025@u.northwestern.edu>
2024-05-21 23:23:36 -0500

    world data cleaning, progress memo

--- 01_data_cleaning.R
+++ 01_data_cleaning.R
@@ -3,41 +3,38 @@
 # load packages 
 library(tidyverse)
 library(here)
-library(stringr)
 
-health_raw <- read_csv(here("data/chicago_health_atlas_may.csv")) |> 
-  janitor::clean_names()
-  
-census_raw <- read_csv(here("data/cook_county_census.csv")) |> 
-  janitor::clean_names() 
+# load data 
 
-census_clean <- census_raw |> 
-  select(name, dp1_0078p, dp1_0079p, dp1_0080p, dp1_0081p, dp1_0082p, dp1_0083p,
-         dp1_0084p) |> 
-  rename(census_tract = name,
-         pct_white = dp1_0078p, 
-         pct_black = dp1_0079p,
-         pct_ai_an = dp1_0080p, 
-         pct_asian = dp1_0081p,
-         pct_nh_pi = dp1_0082p,
-         pct_other = dp1_0083p,
-         pct_multiracial = dp1_0084p
-         ) |> 
-  mutate(census_tract = paste("Tract", 
-                              str_extract(census_tract,"\\d+\\.\\d*|\\d+")))
+world_data <- read_csv(here("data/world-data-2023.csv")) |> 
+  janitor::clean_names() |> 
+  mutate(
+    agricultural_land_percent = str_remove(agricultural_land_percent, "%") 
+      |> as.numeric(),
+    cpi_change_percent = str_remove(cpi_change_percent, "%") 
+      |> as.numeric(),
+    forested_area_percent = str_remove(forested_area_percent, "%") 
+      |> as.numeric(),
+    gross_primary_education_enrollment_percent = str_remove(gross_primary_education_enrollment_percent, "%") 
+      |> as.numeric(),
+    gross_tertiary_education_enrollment_percent = str_remove(gross_tertiary_education_enrollment_percent, "%") 
+      |> as.numeric(),
+    out_of_pocket_health_expenditure = str_remove(out_of_pocket_health_expenditure, "%") 
+    |> as.numeric(),
+    population_labor_force_participation_percent = str_remove(population_labor_force_participation_percent, "%") 
+    |> as.numeric(),
+    tax_revenue_percent = str_remove(tax_revenue_percent, "%") 
+    |> as.numeric(),
+    total_tax_rate = str_remove(total_tax_rate, "%") 
+    |> as.numeric(),
+    unemployment_rate = str_remove(unemployment_rate, "%") 
+    |> as.numeric()#,
+    #minimum_wage = str_remove(minimum_wage, "$") |> as.numeric()
+  )
 
-census <- census_clean[-c(0:2), ]
+    
 
-health_clean <- health_raw |> 
-  select(-c(layer, geoid)) |> 
-  rename(census_tract = name,
-         rent_burden = rbs_2018_2022,
-         chi_eji = chaixyp_2023,
-         food_insec = fai_2020,
-         low_food = lfa_2019,
-         hs_grad = edb_2018_2022,
-         col_grad = ede_2018_2022)
+world_data |> 
+  skimr::skim_without_charts()
 
-# join data 
-map_data <- left_join(census, health_clean, by = "census_tract")
- 
+naniar::gg_miss_var(world_data) 
