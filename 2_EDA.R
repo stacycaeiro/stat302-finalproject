c85c1f0567f7f9d723a54a00c27ee284abe8cd28
stacycaeiro <stacycaeiro2025@u.northwestern.edu>
2024-06-05 16:34:32 -0500

    finished app, created report template

--- 2_EDA.R
+++ 2_EDA.R
@@ -8,8 +8,15 @@ library(here)
 ## load data 
 hh_data <- read_rds("data/holc_health_combined_data_final.rds")
 
-ggplot(hh_data, aes(x = class, y = rbs_2017_2021)) +
+ggplot(hh_data, aes(x = class, y = lng_2021)) +
   geom_boxplot()
 
-ggplot(hh_data, aes(x = pct_w_2017_2021, y = rbs_2017_2021)) +
+ggplot(hh_data, aes(x = pct_w_2017_2021, y = lng_2021)) +
   geom_point()
+
+#uns 
+# median income 
+# poverty rate
+# food insecurity
+#lng 
+
--- shiny-app/app.R
+++ shiny-app/app.R
@@ -1,4 +1,4 @@
-## STAT 302 Final Project 
+## STAT 302 Final Project
 ## Shiny App
 
 # load packages ----
@@ -8,13 +8,31 @@ library(bslib)
 library(here)
 
 # read data ----
-hh_data <- read_rds(here("shiny-app/shiny-data/holc_health_combined_data_final.rds"))
+hh_data <-
+  read_rds(here("shiny-app/shiny-data/holc_health_combined_data_final.rds")) |>
+  mutate(class = factor(
+    class,
+    levels = c("A", "B", "C", "D", "NA"),
+    labels = c(
+      "Best",
+      "Still Desirable",
+      "Definitely Declining",
+      "Hazardous",
+      "NA"
+    )
+  ))
+
+caption_data <- read_csv(here("data/chicago_health_atlas_codebook.csv"))
 
 # Define UI for application that draws a histogram
 ui <- page_sidebar(
   # Application title
-  title = tags$h1(tags$b("Legacy of Neighborhood Redlining Associated with Negative
-                         Socioeconomic Outcomes")),
+  title = tags$h1(
+    tags$b(
+      "Legacy of Neighborhood Redlining Associated with Negative Socioeconomic 
+      Outcomes"
+    )
+  ),
   
   # Sidebar with a slider input for number of bins
   sidebar = sidebar(
@@ -27,9 +45,11 @@ ui <- page_sidebar(
       inputId = "y_var",
       label = "Select Socioeconomic Indicator:",
       choices = c(
-        "% Severe Rent Burden",
-        "Chicago Environmental Justice Index",
-        "College Graduation Rate"
+        "Severe Rent Burden Rate",
+        "College Graduation Rate",
+        "Poverty Rate",
+        "Food Insecurity Rate",
+        "Chronic Obstructive Pulmonary Disease Rate"
       ),
       selected = "College Graduation Rate"
     ),
@@ -37,78 +57,121 @@ ui <- page_sidebar(
   
   # two plots
   layout_column_wrap(
-    width = 1/2,
+    width = 1 / 2,
     height = 300,
-    card(
-      full_screen = TRUE,
-      plotOutput(outputId = "boxPlot")
-    ),
-    card(
-      full_screen = TRUE,
-      plotOutput(outputId = "scatterPlot")
-    )
-  )
+    card(full_screen = TRUE,
+         plotOutput(outputId = "boxPlot")),
+    card(full_screen = TRUE,
+         plotOutput(outputId = "scatterPlot"))
+  ),
+  # explanation of variable & source
+  card(full_screen = TRUE,
+       textOutput(outputId = "varCaption")) #,
+       #textOutput(outputId = "sourceCaption"))
+
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
   output$boxPlot <- renderPlot({
     y_variable <- case_when(
-      input$y_var == "% Severe Rent Burden" ~ "rbs_2017_2021",
-      input$y_var == "Chicago Environmental Justice Index" ~ "chaixyp_2023",
-      input$y_var == "College Graduation Rate" ~ "ede_2017_2021"
+      input$y_var == "Severe Rent Burden Rate" ~ "rbs_2017_2021",
+      input$y_var == "College Graduation Rate" ~ "ede_2017_2021",
+      input$y_var == "Poverty Rate" ~ "pov_2017_2021",
+      input$y_var == "Food Insecurity Rate" ~ "fai_2020",
+      input$y_var == "Chronic Obstructive Pulmonary Disease Rate" ~ "lng_2021"
     )
-
-    # draw boxplot
-    ggplot(data = hh_data,
+    
+    data_boxplot <- hh_data |>
+      filter(class %in% c(
+        "Best",
+        "Still Desirable",
+        "Definitely Declining",
+        "Hazardous"
+      ))
+    
+    ggplot(data = data_boxplot,
            mapping = aes(x = class,
                          y = !!sym(y_variable))) +
       geom_boxplot() +
       theme_minimal() +
-      labs(
-        title = NULL,
-        x = "Class",
-        y = input$y_var
-      ) +
+      labs(title = NULL,
+           x = "Neighborhood Loan Risk Rating",
+           y = input$y_var) +
       theme(
         plot.title = element_text(face = "bold", size = 30),
         legend.position = "inside",
         legend.position.inside = c(0.5, 0.75),
-        plot.background = element_rect(fill = "lightgray"),
         panel.grid.minor = element_blank(),
-        panel.grid.major = element_line(color = "darkgray")
+        axis.text.x = element_text(angle = 45, hjust = 1)
       )
   })
   
   output$scatterPlot <- renderPlot({
     y_variable <- case_when(
-      input$y_var == "% Severe Rent Burden" ~ "rbs_2017_2021",
-      input$y_var == "Chicago Environmental Justice Index" ~ "chaixyp_2023",
-      input$y_var == "College Graduation Rate" ~ "ede_2017_2021"
+      input$y_var == "Severe Rent Burden Rate" ~ "rbs_2017_2021",
+      input$y_var == "College Graduation Rate" ~ "ede_2017_2021",
+      input$y_var == "Poverty Rate" ~ "pov_2017_2021",
+      input$y_var == "Food Insecurity Rate" ~ "fai_2020",
+      input$y_var == "Chronic Obstructive Pulmonary Disease Rate" ~ "lng_2021"
     )
     
-    # draw boxplot
     ggplot(data = hh_data,
            mapping = aes(x = pct_w_2017_2021,
                          y = !!sym(y_variable))) +
       geom_point() +
       theme_minimal() +
-      labs(
-        title = NULL,
-        x = "Non-Hispanic White Residents (%)",
-        y = input$y_var
-      ) +
+      labs(title = NULL,
+           x = "% White Residents in Neighborhood",
+           y = input$y_var) +
       theme(
         plot.title = element_text(face = "bold", size = 30),
         legend.position = "inside",
         legend.position.inside = c(0.5, 0.75),
-        plot.background = element_rect(fill = "lightgray"),
-        panel.grid.minor = element_blank(),
-        panel.grid.major = element_line(color = "darkgray")
+        panel.grid.minor = element_blank()
       )
   })
+  
+  output$varCaption <- renderText({
+    caption_text <- case_when(
+      input$y_var == "Severe Rent Burden Rate" ~ caption_data$rbs_2017_2021[2],
+      input$y_var == "College Graduation Rate" ~ caption_data$ede_2017_2021[2],
+      input$y_var == "Poverty Rate" ~ caption_data$pov_2017_2021[2],
+      input$y_var == "Food Insecurity Rate" ~ caption_data$fai_2020[2],
+      input$y_var == "Chronic Obstructive Pulmonary Disease Rate" ~ caption_data$lng_2021[2]
+    )
+    source_text <- case_when(
+      input$y_var == "Severe Rent Burden Rate" ~ caption_data$rbs_2017_2021[3],
+      input$y_var == "College Graduation Rate" ~ caption_data$ede_2017_2021[3],
+      input$y_var == "Poverty Rate" ~ caption_data$pov_2017_2021[3],
+      input$y_var == "Food Insecurity Rate" ~ caption_data$fai_2020[3],
+      input$y_var == "Chronic Obstructive Pulmonary Disease Rate" ~ caption_data$lng_2021[3]
+    )
+    
+    HTML(paste(input$y_var, 
+               ": ", 
+               caption_text,
+               " [Source: ",
+               source_text,
+               "]",
+               sep = ""))
+  })
+  
+    #output$sourceCaption <- renderText({
+     # source_text <- case_when(
+     #   input$y_var == "Severe Rent Burden Rate" ~ caption_data$rbs_2017_2021[3],
+     #   input$y_var == "College Graduation Rate" ~ caption_data$ede_2017_2021[3],
+     #   input$y_var == "Poverty Rate" ~ caption_data$pov_2017_2021[3],
+     #   input$y_var == "Food Insecurity Rate" ~ caption_data$fai_2020[3],
+     #   input$y_var == "Chronic Obstructive Pulmonary Disease Rate" ~ caption_data$lng_2021[3]
+     # )
+      
+      #HTML(paste("Source: ", 
+      #           source_text,
+       #          sep = ""))
+
 }
 
-# Run the application 
+
+# Run the application
 shinyApp(ui = ui, server = server)
